3aaa5c0fff67519d9d21e8aff1378d40ea86bd3c
Asen Trichkov <asen.trichkov1@gmail.com>
2018-05-30 14:59:52 +0300

    Update transliteration.php

--- transliteration.php
+++ transliteration.php
@@ -1,8 +1,8 @@
 <?php
 	function transliterate($string) {
 		$utf8Str = mb_convert_encoding($string, 'UTF-8', 'Windows-1251');
-		$english = array('a', 'b', 'v', 'g', 'd', 'e', 'j', 'z', 'i', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'sh', 'sht', 'y', 'iu', 'ia');
-		$cyrillic = array('а', 'б', 'в','г', 'д', 'е', 'ж', 'з', 'и' ,'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'x', 'ц', 'ш', 'щ', 'ъ', 'ю', 'я');
+		$english = array('a', 'b', 'v', 'g', 'd', 'e', 'j', 'z', 'i', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'sh', 'ch', 'sht', 'y', 'iu', 'ia');
+		$cyrillic = array('а', 'б', 'в' ,'г' , 'д', 'е', 'ж', 'з','и','й','к','л','м','н','о','п','р','с','т','у','ф','x','ц', 'ч', 'ш','щ', 'ъ', 'ю', 'я');
 		$result = str_replace($cyrillic, $english, mb_strtolower($utf8Str));
 
 		return $result;
